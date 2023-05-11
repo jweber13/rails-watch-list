11b173a7ea3d081af142d1143ec71ada9aacd7ff
joe weber <jweber4213@gmail.com>
2023-05-11 15:59:10 +0900

    first draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning the db....'
+List.destroy_all
+Movie.destroy_all
+Bookmark.destroy_all
+
+puts "Creating Lists"
+5.times do
+  List.create!(
+    name: Faker::IndustrySegments.sub_sector
+  )
+end
+
+puts "Creating Movies"
+15.times do |i|
+  Movie.create!(
+    title: Faker::Movie.title, # + "#{i}",
+    overview: Faker::Movie.quote,
+    rating: rand(1..6),
+    poster_url: "http://source.unsplash.com/featured/?cinema>&#{rand(100)}"
+  )
+end
+
+puts "Making Bookmark"
+m_list = Movie.all.each_slice(3).to_a
+l_list = List.all
+l_list.each_with_index do |li, idx|
+  m_list[idx].each do |m|
+    Bookmark.create!(
+      comment: Faker::Books::Lovecraft.sentence,
+      movie: m,
+      list: li
+    )
+  end
+end
+
+# Bookmark.create!(
+#   comment: "This is a comment",
+#   movie: Movie.first,
+#   list: List.first
+# )
