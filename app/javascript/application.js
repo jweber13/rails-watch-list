e303c7fd45e722681f808526a45cfcda89a384f4
joe weber <jweber4213@gmail.com>
2023-05-12 15:30:48 +0900

    updated ui

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+
+document.querySelector(".card-hello img")
+  .addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle("tapped");
+  });
+
+document.querySelector(".item-drawer-button")
+  .addEventListener("click", (event) => {
+    document.querySelector(".item-drawer").classList.toggle("active");
+  });
